c4848c97b568d16dd7493f0642656bc443121cf9
Nevyan Plamenov <plamenovnevyan@gmail.com>
2023-08-14 16:15:16 +0300

    Added typewriting animation for one of the headeings
    Also added header color swap animations when scrolling

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
     let controller = new ScrollMagic.Controller({
         globalSceneOptions: {duration: 538}
     })
+    let typeWriteScene = initTypewritingScene().addTo(controller)
+    let headerScenes = initHeaderBgScenes().forEach(scene => scene.addTo(controller)) 
     let sceneImg1 = new ScrollMagic.Scene({
         triggerElement: '.article-right-top'
     })
@@ -17,4 +19,50 @@ $(document).ready(function(){
     })
     .setClassToggle('#article-right-img-bottom', 'fade-in')
     .addTo(controller)
+
+    function initTypewritingScene(){
+        let textToDisplay = ' We are a complete Talent Pool solution that combines HR tech, marketing automation, real human interactions and the power of your brand to attract, engage and manage curated talent pools'
+        let textLength = textToDisplay.length
+    
+        function typing(lengthToDisplay){
+            if(lengthToDisplay <= textLength){
+                $('#type-anim-text').text(textToDisplay.substring(0, lengthToDisplay))
+            }
+        }
+    
+        let typewriteOnScroll = new TimelineMax()
+        let typewriteScene = new ScrollMagic.Scene({
+            triggerElement :'.advert-section-3',
+            duration: 500
+        }).on('progress', function(){
+            let scrollProgress = Math.ceil(typewriteScene.progress() * textLength)
+            typing(scrollProgress)
+        })
+    
+        .setPin('#type-anim-text')
+        .setTween(typewriteOnScroll)
+        return typewriteScene
+    }
+
+    function initHeaderBgScenes(){
+        let firstChange = new ScrollMagic.Scene({
+            triggerElement : '.advert-section-2',
+            duration: 300
+        })
+        .setClassToggle('#page-header', 'bg-blue')
+
+        let secondChange = new ScrollMagic.Scene({
+            triggerElement : '.advert-section-4',
+            duration: 300
+        })
+        .setClassToggle('#page-header', 'bg-green')
+
+        let thirdChange = new ScrollMagic.Scene({
+            triggerElement : '.details-section',
+            duration: 300
+        })
+        .setClassToggle('#page-header', 'bg-gradient')
+
+        return [firstChange, secondChange, thirdChange]
+    }
 })
