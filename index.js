01816337f3640b1c57936b0051a23443616786ee
Nevyan Plamenov <plamenovnevyan@gmail.com>
2023-08-16 09:46:33 +0300

    Last commit, added responsiveness and additional animations

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
 $(document).ready(function(){
     let controller = new ScrollMagic.Controller({
-        globalSceneOptions: {duration: 538}
+        globalSceneOptions: {duration: 538},
+        triggerHook: 'onLeave'
     })
     let typeWriteScene = initTypewritingScene().addTo(controller)
     let headerScenes = initHeaderBgScenes().forEach(scene => scene.addTo(controller)) 
-    let sceneImg1 = new ScrollMagic.Scene({
-        triggerElement: '.article-right-top'
-    })
-    .setClassToggle('#article-right-img-top', 'fade-in')
-    .addTo(controller)
-    let sceneImg2 = new ScrollMagic.Scene({
-        triggerElement: '.article-left-bottom'
-    })
-    .setClassToggle('#article-left-img', 'fade-in')
-    .addTo(controller)
-    let sceneImg3 = new ScrollMagic.Scene({
-        triggerElement: '.article-right-bottom'
-    })
-    .setClassToggle('#article-right-img-bottom', 'fade-in')
-    .addTo(controller)
-
+    let sponsorArticlesScene = initSponsorsScene().addTo(controller)
+    let workforceLiScene = initWorkforceLiScene().addTo(controller)
+    let detailsImgScenes = initDetailsImgScenes().forEach(scene => scene.addTo(controller)) 
+    
     function initTypewritingScene(){
         let textToDisplay = ' We are a complete Talent Pool solution that combines HR tech, marketing automation, real human interactions and the power of your brand to attract, engage and manage curated talent pools'
         let textLength = textToDisplay.length
@@ -64,5 +53,52 @@ $(document).ready(function(){
         .setClassToggle('#page-header', 'bg-gradient')
 
         return [firstChange, secondChange, thirdChange]
+    }
+    function initSponsorsScene(){
+        let tl = new TimelineMax()
+        tl.staggerFrom('.sponsor-article',1.5, {
+            scale: 0,
+            cycle: {
+                y: [-50, 50]
+            },
+            stagger: {
+                from: "center",
+                amount: 0.35
+            }
+        })
+        let sponsorArticlesScene = new ScrollMagic.Scene({
+            triggerElement: '.sponsor-articles',
+            duration: 300
+        }) 
+        .setTween(tl)
+        return sponsorArticlesScene
+    }
+    function initWorkforceLiScene(){
+        let workforceLiScene = new ScrollMagic.Scene({
+            triggerElement : '.about-section',
+            duration: 500,
+            // triggerHook: 'onEnter'
+        })
+        .setClassToggle('.workforce-list', 'workforce-list-scale')
+
+        return workforceLiScene
+    }
+    function initDetailsImgScenes(){
+        let sceneImg1 = new ScrollMagic.Scene({
+            triggerElement: '.article-right-top'
+        })
+        .setClassToggle('#article-right-img-top', 'fade-in')
+        .addTo(controller)
+        let sceneImg2 = new ScrollMagic.Scene({
+            triggerElement: '.article-left-bottom'
+        })
+        .setClassToggle('#article-left-img', 'fade-in')
+        .addTo(controller)
+        let sceneImg3 = new ScrollMagic.Scene({
+            triggerElement: '.article-right-bottom'
+        })
+        .setClassToggle('#article-right-img-bottom', 'fade-in')
+
+        return [ sceneImg1, sceneImg2, sceneImg3]
     }
 })
